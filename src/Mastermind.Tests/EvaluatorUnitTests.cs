20b6852c93f2c5526400ea72f4bb2933bafae7a6
Aileen Morgan <amorgan101010@protonmail.com>
2022-03-19 17:32:26 -0400

    Test reorg

--- src/Mastermind.Tests/EvaluatorUnitTests.cs
+++ src/Mastermind.Tests/EvaluatorUnitTests.cs
@@ -10,8 +10,8 @@ public class EvaluatorUnitTests
     [Fact]
     public void HandlesGuess_WithNoCattle()
     {
-        var guess = new List<int>() { 21, 5, 7, 2, 1 };
         var secret = new List<int>() { 22, 6, 8, 3, 3 };
+        var guess = new List<int>() { 21, 5, 7, 2, 1 };
         var expected = new GuessEvaluation(0, 0);
 
         var sut = new Evaluator();
@@ -28,7 +28,6 @@ public class EvaluatorUnitTests
     {
         var secret = new List<int>() { 22, 6, 8, 3, 3 };
         var guess = secret;
-
         var expected = new GuessEvaluation(5, 0);
 
         var sut = new Evaluator();
@@ -43,8 +42,8 @@ public class EvaluatorUnitTests
     [Fact]
     public void HandlesGuess_WithSingleBull()
     {
-        var guess = new List<int>() { 22, 5, 7, 2, 1 };
         var secret = new List<int>() { 22, 6, 8, 3, 3 };
+        var guess = new List<int>() { 22, 5, 7, 2, 1 };
         var expected = new GuessEvaluation(1, 0);
 
         var sut = new Evaluator();
@@ -59,8 +58,8 @@ public class EvaluatorUnitTests
     [Fact]
     public void HandlesGuess_WithSingleCow()
     {
-        var guess = new List<int>() { 5, 22, 7, 2, 1 };
         var secret = new List<int>() { 22, 6, 8, 3, 3 };
+        var guess = new List<int>() { 5, 22, 7, 2, 1 };
         var expected = new GuessEvaluation(0, 1);
 
         var sut = new Evaluator();
@@ -75,8 +74,8 @@ public class EvaluatorUnitTests
     [Fact]
     public void HandlesGuess_WithCow_SupercededByLaterMatchingBull()
     {
-        var guess = new List<int>() { 8, 0, 8, 2, 1 };
         var secret = new List<int>() { 22, 6, 8, 3, 3 };
+        var guess = new List<int>() { 8, 0, 8, 2, 1 };
         var expected = new GuessEvaluation(1, 0);
 
         var sut = new Evaluator();
@@ -91,8 +90,8 @@ public class EvaluatorUnitTests
     [Fact]
     public void HandlesGuess_WithBull_OverridingLaterMatchingCow()
     {
-        var guess = new List<int>() { 22, 22, 7, 2, 1 };
         var secret = new List<int>() { 22, 6, 8, 3, 3 };
+        var guess = new List<int>() { 22, 22, 7, 2, 1 };
         var expected = new GuessEvaluation(1, 0);
 
         var sut = new Evaluator();
@@ -107,8 +106,8 @@ public class EvaluatorUnitTests
     [Fact]
     public void HandlesGuess_WithSingleBull_OverridingManyMatchingCows()
     {
-        var guess = new List<int>() { 8, 8, 8, 8, 1 };
         var secret = new List<int>() { 22, 6, 8, 3, 3 };
+        var guess = new List<int>() { 8, 8, 8, 8, 1 };
         var expected = new GuessEvaluation(1, 0);
 
         var sut = new Evaluator();
@@ -123,8 +122,8 @@ public class EvaluatorUnitTests
     [Fact]
     public void HandlesGuess_WithMoreDuplicateCows_ThanMatchingDuplicateElementsInSecret()
     {
-        var guess = new List<int>() { 3, 3, 3, 7, 1 };
         var secret = new List<int>() { 22, 6, 8, 3, 3 };
+        var guess = new List<int>() { 3, 3, 3, 7, 1 };
         var expected = new GuessEvaluation(0, 2);
 
         var sut = new Evaluator();
@@ -139,8 +138,8 @@ public class EvaluatorUnitTests
     [Fact]
     public void HandlesGuess_WithFewerDuplicateCows_ThanMatchingDuplicateElementsInSecret()
     {
-        var guess = new List<int>() { 3, 3, 7, 7, 1 };
         var secret = new List<int>() { 6, 6, 3, 3, 3 };
+        var guess = new List<int>() { 3, 3, 7, 7, 1 };
         var expected = new GuessEvaluation(0, 2);
 
         var sut = new Evaluator();
@@ -155,8 +154,8 @@ public class EvaluatorUnitTests
     [Fact]
     public void HandlesGuess_WithMoreDuplicateCows_ThanMatchingDuplicateElementsInSecret_AndMatchingBull()
     {
-        var guess = new List<int>() { 1, 3, 3, 3, 1 };
         var secret = new List<int>() { 22, 6, 8, 3, 3 };
+        var guess = new List<int>() { 1, 3, 3, 3, 1 };
         var expected = new GuessEvaluation(1, 1);
 
         var sut = new Evaluator();
@@ -171,8 +170,8 @@ public class EvaluatorUnitTests
     [Fact]
     public void HandlesGuess_WithFewerDuplicateCows_ThanMatchingDuplicateElementsInSecret_AndMatchingBull()
     {
-        var guess = new List<int>() { 1, 3, 3, 7, 1 };
         var secret = new List<int>() { 6, 6, 3, 3, 3 };
+        var guess = new List<int>() { 1, 3, 3, 7, 1 };
         var expected = new GuessEvaluation(1, 1);
 
         var sut = new Evaluator();
